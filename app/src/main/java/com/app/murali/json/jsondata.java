be9e1e5aff888d402aef08d11c882f7157e69946
Muralimano <muralimanoharsoft@gmail.com>
2019-01-02 09:25:18 +0530

    json parsed data

--- app/src/main/java/com/app/murali/json/jsondata.java
+++ app/src/main/java/com/app/murali/json/jsondata.java
@@ -3,6 +3,10 @@ package com.app.murali.json;
 import android.os.AsyncTask;
 import android.widget.ScrollView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,22 +18,40 @@ import java.net.URL;
 public class jsondata extends AsyncTask<Void, Void, Void> {
 
     String data = "";
+    String dataparsed = "";
+    String datasingleparsed = "";
+
     @Override
     protected Void doInBackground(Void... voids) {
         try {
             URL url = new URL("https://api.myjson.com/bins/1702wk");
-            HttpURLConnection httpURLConnection =(HttpURLConnection) url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
             String line = "";
-            while(line !=null){
+            while (line != null) {
                 line = bufferedReader.readLine();
                 data = data + line;
+            }
+
+            JSONArray jsonArray = new JSONArray(data);
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jo = (JSONObject) jsonArray.get(i);
+
+                datasingleparsed = "Name : " + jo.get("name") + "\n" +
+                        "Phone No : " + jo.get("phoneno") + "\n" +
+                        "Address : " + jo.get("address") + "\n" +
+                        "Email : " + jo.get("email") + "\n";
+
+                dataparsed = dataparsed + datasingleparsed + "\n";
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -39,6 +61,6 @@ public class jsondata extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        MainActivity.getjson1.setText(this.data);
+        MainActivity.getjson1.setText(this.dataparsed);
     }
 }
